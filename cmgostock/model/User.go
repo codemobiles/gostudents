0cc0599be47c85a042795e21b2f8beced861fef4
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 15:00:34 +0700

    1234

--- cmgostock/model/User.go
+++ cmgostock/model/User.go
@@ -5,9 +5,9 @@ import "time"
 // https://gorm.io/docs/models.html
 
 type User struct {
-	ID        uint
-	Username  string
-	Password  string
+	ID        uint   `gorm:"primary_key"`
+	Username  string `gorm:"unique" form:"username" binding:"required"`
+	Password  string `form:"password" binding:"required"`
 	Level     string `gorm:"default:normal"`
 	CreatedAt time.Time
 }
