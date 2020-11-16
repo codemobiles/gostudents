942a1a9adc762c3f3e096bd4701d125d57661bb9
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 10:16:42 +0700

    1234

--- demo1/index.go
+++ demo1/index.go
@@ -13,9 +13,19 @@ func main() {
 	// t5 = "yyy"
 
 	fmt.Println(t1, t2, t3, t4, t5)
-	fn1()
+	fn1("lek")
+	fn2("hello", 11, false)
 }
 
-func fn1()  {
-	fmt.Printf("Hey %s\n", "lek")
+func fn1(p1 string) {
+	fmt.Printf("Hey %s\n", p1)
+}
+
+func fn2(p1 string, p2 int, p3 bool) {
+	fmt.Printf("Hey %s, %d, %v\n", p1, p2, p3)
+}
+
+func fn3(p1 string, p2 int, p3 bool) string {
+	fmt.Printf("Hey %s, %d, %v\n", p1, p2, p3)
+	return "I am return function"
 }
