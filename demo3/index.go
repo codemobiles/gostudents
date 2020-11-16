ba5d0a814096cbe8ed0cb425f9bc854193d592cd
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 11:48:46 +0700

    1234

--- demo3/index.go
+++ demo3/index.go
@@ -6,4 +6,20 @@ func main() {
 	fmt.Println("Pointer")
 	d1 := "Lek"
 	fmt.Println(d1)
+
+	var d1Pointer *string
+	d1Pointer = &d1
+	d1Clone := d1
+
+	*d1Pointer = "Kan"
+	clear(&d1)
+
+	fmt.Println("d1Pointer :", *d1Pointer)
+	fmt.Println("d1 :", d1)
+	fmt.Println("d1Clone :", d1Clone)
+}
+
+
+func clear(v1 *string)  {
+	*v1 = ""
 }
---
+++ demo4/index.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "fmt"
+	_ "net/http"
+
+	_ "github.com/gin-gonic/gin"
+)
+
+func main() {
+
+}
