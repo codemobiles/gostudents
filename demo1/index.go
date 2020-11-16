b79b7cc25bdb7130ea98ffe250ea4f8c5317cd88
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 10:12:18 +0700

    1234

--- demo1/index.go
+++ demo1/index.go
@@ -6,7 +6,16 @@ func main() {
 	fmt.Println("Hello World")
 	var t1 string = "test"
 	var t2 int = 10
-	
+	var t3 bool = true
+	t4 := "lek"
+	t1 = "codemobiles"
+	const t5 string = "xxx"
+	// t5 = "yyy"
 
-	fmt.Println(t1, t2)
+	fmt.Println(t1, t2, t3, t4, t5)
+	fn1()
+}
+
+func fn1()  {
+	fmt.Printf("Hey %s\n", "lek")
 }
