e6313a86dfc99ec8c1acabc09bd70fe90e3432d6
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 13:43:41 +0700

    1234

--- cmgostock/api/api.go
+++ cmgostock/api/api.go
@@ -1,14 +1,12 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 // Setup -- Call this method to setup gin router
 func Setup(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hey")
-	})
+	SetupAuthenAPI(router)
+	SetupProductAPI(router)
+	SetupTransactionAPI(router)
 }
--- cmgostock/api/authen.api.go
+++ cmgostock/api/authen.api.go
@@ -1,5 +1,22 @@
 package api
 
-func SetupAuthenAPI(router *gin.Engine) {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+// SetupAuthenAPI - Call this method to setup authen api
+func SetupAuthenAPI(router *gin.Engine) {
+	authenAPI := router.Group("/api/v2")
+	authenAPI.POST("/login", login)
+	authenAPI.POST("/register", register)
+}
+
+func login(c *gin.Context) {
+	c.String(http.StatusOK, "Login")
+}
+
+func register(c *gin.Context) {
+	c.String(http.StatusOK, "Register")
 }
