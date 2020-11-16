fc0b83ddb156ef55281a42020f4166d587d7a981
codemobiles <chaiyasit.t@gmail.com>
2020-11-16 15:45:49 +0700

    1234

--- cmgostock/interceptor/general.interceptor.go
+++ cmgostock/interceptor/general.interceptor.go
@@ -1,9 +1,18 @@
 package interceptor
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func VerifyIt(c *gin.Context) {
-	c.Next()
+
+	if token := c.Query("token"); token == "1234" {
+		c.Next()
+	} else {
+		c.String(http.StatusOK, "Error no token")
+		c.Abort()
+	}
+
 }
