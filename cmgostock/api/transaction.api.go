01c20e91f79db3fc7b4d186acb8377fe57090698
codemobiles <chaiyasit.t@gmail.com>
2020-11-17 11:16:44 +0700

    1234

--- cmgostock/api/transaction.api.go
+++ cmgostock/api/transaction.api.go
@@ -14,8 +14,21 @@ func SetupTransactionAPI(router *gin.Engine) {
 	}
 }
 
+type TransactionResult struct {
+	Change        float64 `json:"change"`
+	CreatedAt     string  `json:"created_at"`
+	ID            uint    `json:"id"`
+	OrderList     string  `json:"order_list"`
+	Paid          float64 `json:"paid"`
+	PaymentDetail string  `json:"payment_detail"`
+	PaymentType   string  `json:"payment_type"`
+	Total         float64 `json:"total"`
+	Staff         string  `json:"staff_id"`
+}
+
 func getTransaction(c *gin.Context) {
-	var result []map[string]interface{}
+	// var result []map[string]interface{}
+	var result []TransactionResult
 	db.GetDB().Debug().Raw("SELECT transactions.id, total, paid, change, payment_type, payment_detail, order_list, users.username as Staff, transactions.created_at FROM transactions join users on transactions.staff_id = users.id order by transactions.created_at DESC", nil).Scan(&result)
 	c.JSON(200, result)
 }
