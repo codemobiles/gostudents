1a6c5142859b8aa31e36474fd9a20f91fa290817
codemobiles <chaiyasit.t@gmail.com>
2020-11-17 11:37:51 +0700

    1234

--- cmgostock/server.go
+++ cmgostock/server.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"main/api"
+	"time"
 	_ "time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/itsjamie/gin-cors"
+	cors "github.com/itsjamie/gin-cors"
 )
 
 func main() {
 	router := gin.Default()
-	router.Static("/images", "./uploaded/images")
 
+	// Set up CORS middleware options
+	config := cors.Config{
+		Origins:        "*",
+		RequestHeaders: "Origin, Authorization, Content-Type",
+
+		Methods:         "GET, POST, PUT, DELETE",
+		Credentials:     true,
+		ValidateHeaders: false,
+		MaxAge:          1 * time.Minute,
+	}
+
+	// Apply the middleware to the router (works on groups too)
+	router.Use(cors.Middleware(config))
+	router.Static("/images", "./uploaded/images")
 	api.Setup(router)
 	router.Run(":8081")
 }
