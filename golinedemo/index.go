3012bb19d1095202c3811b3aa12eda8d01cfa706
codemobiles <chaiyasit.t@gmail.com>
2020-11-17 15:27:26 +0700

    1324

--- golinedemo/index.go
+++ golinedemo/index.go
@@ -1,5 +1,16 @@
 package main
-import _ "github.com/utahta/go-linenotify"
+
+import (
+	"bytes"
+
+	"github.com/utahta/go-linenotify"
+)
+
 func main() {
-	
+	token := "" // EDIT THIS
+
+	c := linenotify.New()
+	c.Notify(token, "hello world", "", "", nil)
+	c.Notify(token, "hello world", "http://localhost/thumb.jpg", "http://localhost/full.jpg", nil)
+	c.Notify(token, "hello world", "", "", bytes.NewReader([]byte("image bytes")))
 }
