e15df93e9f614cc7a83134bcf3cf2e33ff8f36e9
codemobiles <chaiyasit.t@gmail.com>
2020-11-17 10:18:19 +0700

    1234

--- cmgostock/api/product.api.go
+++ cmgostock/api/product.api.go
@@ -91,3 +91,19 @@ func saveImage(image *multipart.FileHeader, product *model.Product, c *gin.Conte
 		db.GetDB().Model(&product).Update("image", fileName)
 	}
 }
+
+func editProduct(c *gin.Context) {
+	var product model.Product
+	id, _ := strconv.ParseInt(c.PostForm("id"), 10, 32)
+
+	product.ID = uint(id)
+	product.Name = c.PostForm("name")
+	product.Stock, _ = strconv.ParseInt(c.PostForm("stock"), 10, 64)
+	product.Price, _ = strconv.ParseFloat(c.PostForm("price"), 64)
+	product.CreatedAt = time.Now()
+	db.GetDB().Updates(&product)
+
+	image, _ := c.FormFile("image")
+	saveImage(image, &product, c)
+	c.JSON(http.StatusOK, gin.H{"result": product})
+}
