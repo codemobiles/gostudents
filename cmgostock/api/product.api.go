ef95b6e9e20169eaa98bd074ed22f6b65126911c
codemobiles <chaiyasit.t@gmail.com>
2020-11-17 09:37:44 +0700

    1234

--- cmgostock/api/product.api.go
+++ cmgostock/api/product.api.go
@@ -32,9 +32,9 @@ func getProduct(c *gin.Context) {
 	var products []model.Product
 	if keyword := c.Query("keyword"); keyword != "" {
 		keyword = fmt.Sprintf("%%%s%%", keyword)
-		db.GetDB().Where("name like ?", keyword).Find(&products)
+		db.GetDB().Where("name like ?", keyword).Order("created_at DESC").Find(&products)
 	} else {
-		db.GetDB().Find(&products)
+		db.GetDB().Order("created_at DESC").Find(&products)
 	}
 
 	c.JSON(http.StatusOK, products)
